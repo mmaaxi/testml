6b8a2f7d3d46695cf2cf8cb7f058b4e2e74b74da
Maximiliano Reyes <maxyreyes7@gmail.com>
2024-08-31 12:43:44 -0600

    first commit

---
+++ tests/testml.spec.js
@@ -0,0 +1,35 @@
+const { test } = require('@playwright/test');
+
+/*
+1) Enter the website  
+2) Select México as a country  
+3) Search for the term “playstation 5”  
+4) Filter by condition “Nuevos”  
+5) Filter by location “Cdmx”  
+6) Order by “mayor a “menor precio”  
+7) Obtain the name and the price of the first 5 products   
+8) Print these products in the console  
+*/
+
+test('test1', async({page}) => {
+  await page.goto("https://www.mercadolibre.com")
+  await page.getByRole('link', {name: "México"}).click();
+  await page.getByRole('combobox', {name: "Ingresa lo que quieras encontrar"}).fill('playstation 5')
+  await page.getByRole('button', {name: "Buscar"}).click()
+  await page.getByRole('link', { name: /^Nuevo, \d+ resultados$/ }).click();
+  await page.getByRole('link', { name: /^Distrito Federal, \d+ resultados$/ }).click();
+  await page.getByRole('button', { name: "Más relevantes Más relevantes"}).click()
+  await page.getByRole('option', { name: "Mayor precio"}).click()
+
+  const products = await page.$$eval('ol.ui-search-layout.ui-search-layout--stack.shops__layout > li.ui-search-layout__item.shops__layout-item.ui-search-layout__stack', items => 
+    items.slice(0, 5).map(item => ({
+        name: item.querySelector('h2.ui-search-item__title').innerText,
+        price: item.querySelector('span.andes-money-amount').innerText.replace(/\s+/g, ''),
+    }))
+);
+console.log('Top 5 Products:');
+products.forEach((product, index) => {
+    console.log(`${index + 1}. ${product.name} - $${product.price}`);
+});
+})
+
